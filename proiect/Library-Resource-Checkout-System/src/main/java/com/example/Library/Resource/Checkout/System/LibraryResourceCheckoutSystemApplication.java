0453a7419b5dffbae1725dac96eaddf2a13fdbf1
vivienknecht <knechtvivien@gmail.com>
2026-02-22 12:09:55 +0200

    new project

---
+++ proiect/Library-Resource-Checkout-System/src/main/java/com/example/Library/Resource/Checkout/System/LibraryResourceCheckoutSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Library.Resource.Checkout.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryResourceCheckoutSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryResourceCheckoutSystemApplication.class, args);
+	}
+
+}
---
+++ proiect/Library-Resource-Checkout-System/src/test/java/com/example/Library/Resource/Checkout/System/LibraryResourceCheckoutSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Library.Resource.Checkout.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryResourceCheckoutSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
